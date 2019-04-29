f7e69e07213eef116ad3451b69a8f5571d28d322
stathisKara <sdi1200057@di.uoa.gr>
2019-04-29 16:03:40 -0400

    added output file, 1000000 inserts of which 10% are updates and 10% are deletes also creates 500000 get queries with 80% of them existing in the tree

--- inputs/createInput.cpp
+++ inputs/createInput.cpp
@@ -7,24 +7,46 @@
 #include <string>
 #include <cstdio>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     int lines = stoi(argv[1]);
-    printf("%d\n", lines);
+    printf("%d\n", lines + lines / 2);
     srand(time(NULL));
 
 
     int dels = stoi(argv[2]);
-    int upds = stoi(argv[3]) * lines;
-    int existGets;
-    int nonExistGets;
+    int upds = stoi(argv[3]);
+    int existGets = stoi(argv[4]);
+    int nonExistGets = 100 - existGets;
+
+    vector<int> g1;
 
     for (int i = 0; i < lines; i++) {
-        if (rand()% 100 < dels)
-            printf("d %d\n", rand() % (2 * i));
-        else
-            printf("i %d %d\n", i, i);
+        if (rand() % 100 < dels + upds) {
+            if (rand() % 2 == 0)
+                printf("r %d\n", g1[rand() % (g1.size())]);
+            else {
+                if (g1.size() != 0) {
+                    printf("u %d %d\n", g1[rand() % g1.size()], rand() % lines);
+                }
+            }
+        }
+        else {
+            int elem = rand() % (lines * 100);
+            g1.push_back(elem);
+            printf("i %d %d\n", elem, rand() % lines);
+        }
+    }
+
+    for (int i = 0; i < lines / 2; i++) {
+        if (rand() % 100 < existGets) {
+            printf("g %d\n", g1[rand() % g1.size()]);
+        }
+        else {
+            printf("g %d %d\n", rand() % (lines * 100));
+        }
     }
 }
