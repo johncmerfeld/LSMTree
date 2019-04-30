77455b3369c29582f960537702e2a53347704485
John Merfeld <john.merfeld@gmail.com>
2019-04-29 23:25:26 -0400

    plotting code

--- plotResults.R
+++ plotResults.R
@@ -4,9 +4,19 @@ library(ggplot2)
 data <- fread("results.csv")
 
 ggplot(data,
-       aes(x = V1,
-           y = V2)) +
+       aes(x = memrun_size,
+           y = insertion_time_millisecs / 1000)) +
   geom_point(size = 5) + 
   geom_line() +
-  xlab("Size of run") + 
-  ylab("Time (ms)")
+  xlab("Number of entries in the memory run") + 
+  ylab("Time (s)") +
+  ggtitle("Larger memory runs improve write performance")
+
+ggplot(data,
+       aes(x = memrun_size,
+           y = gets_millisecs / 1000)) +
+  geom_point(size = 5) + 
+  geom_line() +
+  xlab("Number of entries in the memory run") + 
+  ylab("Time (s)") +
+  ggtitle("The relationship between run size and read performance is less clear")
