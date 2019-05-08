f656b8909bb9ef2ecd7345df1e3a2c3d726eb60a
stathisKara <sdi1200057@di.uoa.gr>
2019-05-07 22:22:50 -0400

    Final Polish

--- Main.cpp
+++ Main.cpp
@@ -12,14 +12,14 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    ResultSet *rs;
-    TierLsmTree *tree = new TierLsmTree(4000, 2, 5);
-
-    for (int i = 0; i < atoi(argv[1]); i++) {
-        tree->insert(i, i);
-    }
-
-    tree->get(3999)->printer();
-    tree->getRange(10, 50)->printer();
-}
+//int main(int argc, char *argv[]) {
+//    ResultSet *rs;
+//    TierLsmTree *tree = new TierLsmTree(4000, 2, 5);
+//
+//    for (int i = 0; i < atoi(argv[1]); i++) {
+//        tree->insert(i, i);
+//    }
+//
+//    tree->get(3999)->printer();
+//    tree->getRange(10, 50)->printer();
+//}
